594625eecd6fddd29b69d41c6e52b8ed8c4a347d
Andrew Church <andrewchurch@Andrews-MacBook-Pro.local>
2017-09-04 21:56:19 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var app = express()
+var HTTP = require('http')
+var HTTPS = require('https')
+var fs = require('fs')
+
+
+app.get('/', function(req, res){
+    console.log("we're logging on the server!")
+    res.send("<h1>Welcome to the internet!</h1>")
+})
+
+try {
+    var httpsConfig = {
+        key  : fs.readFileSync('/etc/letsencrypt/live/aconlineventures.com/privkey.pem'),
+        cert : fs.readFileSync('/etc/letsencrypt/live/aconlineventures.com/cert.pem')
+    }
+    var httpsServer = HTTPS.createServer(httpsConfig, app)
+    httpsServer.listen(443)
+}
+catch(error){
+    console.log(error)
+    console.log('could not set up HTTPS')
+}
+finally {
+    console.log('this code runs regardless of whether the above code succeeded or failed')
+}
+
+var httpApp = express()
+httpApp.use(function(req, res){
+    console.log(req.url)
+    res.redirect('https://aconlineventures.com' + req.url)
+})
+httpApp.listen(80)
